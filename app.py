7c6adb935c2ac493fb3ac5ec58cf46e3b8aa5607
Dan Poulsen <dangjogvara@hotmail.com>
2020-09-28 19:54:13 +0100

    add jwt_required decorator to get function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from flask_jwt import JWT
+from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 
@@ -14,6 +14,7 @@ items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
