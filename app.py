d1a69755b1c7b10c2031876f7b8310a69d838918
Dan Poulsen <dangjogvara@hotmail.com>
2020-09-28 20:49:41 +0100

    Section 77

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=float, required=True, help="This field cannot be left blank!")
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,7 +26,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': f"An item with '{name}' already exists."}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -34,9 +38,7 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
 
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
