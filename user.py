e6838f834f90dd094d0c2a9ebad9fb7764cf0689
Dan Poulsen <dangjogvara@hotmail.com>
2020-10-01 20:20:19 +0100

    section 83

--- user.py
+++ user.py
@@ -50,6 +50,9 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
+        if User.find_by_username(data['username'] is not None):
+            return {"message": "A user with that username already exists."}, 400
+
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
