a83d4c1f86c70382fe4da47149c6d59d1ef78704
Dan Poulsen <dangjogvara@hotmail.com>
2020-10-05 21:02:08 +0100

    section 85

--- item.py
+++ item.py
@@ -5,10 +5,18 @@ from flask_jwt import jwt_required
 
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('name', type=float, required=True, help="This field cannot be left blank!")
+    parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -21,10 +29,24 @@ class Item(Resource):
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
 
-        return {'message': 'Item not found'}
-
     def post(self, name):
-        pass
+        if self.find_by_name(name):
+            return {'message': f"An item with name '{name}' already exists."}, 400
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
+        return item, 201
 
     def delete(self, name):
         pass
