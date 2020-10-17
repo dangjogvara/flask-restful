c9421b2be3dd2d6b1328f423ddaecd70ec998e33
Dan Poulsen <dangjogvara@hotmail.com>
2020-10-17 12:49:15 +0100

    section 98

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+from db import db
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dan'
 api = Api(app)
 
@@ -18,4 +21,5 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=True)
