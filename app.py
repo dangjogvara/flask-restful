e8bbfab2042967a6dc4b113d8ed525e9b1a33aa4
Dan Poulsen <dangjogvara@hotmail.com>
2020-10-18 11:59:52 +0100

    section 99

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dan'
 api = Api(app)
--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    password = db.columd(db.String(80))
+    password = db.column(db.String(80))
 
     def __init__(self, _id, username, password):
         self.id = _id
