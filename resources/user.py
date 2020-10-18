ce8d8c24bff6b2b1a1cdbb33e5474a07503cbc7a
Dan Poulsen <dangjogvara@hotmail.com>
2020-10-18 12:12:35 +0100

    section 100

--- resources/user.py
+++ resources/user.py
@@ -13,16 +13,10 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if UserModel.find_by_username(data['username'] is not None):
+        if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists."}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {"message": "User created succesfully."}, 201
